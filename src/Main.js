f43a765f2e948217cc7c1acb85cb21ea1950de7a
Raziel Einhorn <raziele@gmail.com>
2021-04-10 14:00:55 +0300

    reorg the code under src/

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,16 @@
 function main(){
   const analysisFileId = PropertiesService.getDocumentProperties().getProperty('ID_ANALYSIS_FILE');
   const analysisFile = SpreadsheetApp.openById(analysisFileId);
+  //const analysisFile = SpreadsheetApp.getActiveSpreadsheet();
   const analysisDbSheet = analysisFile.getSheetByName('DB');
   const analysisCategoriesSheet = analysisFile.getSheetByName('Categories');
   const analysisStatusSheet = analysisFile.getSheetByName('_status');
   const idReportFolder = PropertiesService.getDocumentProperties().getProperty('ID_REPORTS_FOLDER');
   const newFiles = get_new_files_by_list(idReportFolder, analysisStatusSheet);
   var categoriesTable = analysisCategoriesSheet.getDataRange().getValues();
-  
+  const transactionDetailsTemplate = new makeStruct("inputDate, fid, fname, type, nCard, billingMonth, transactionDate, name, amount, currency");
+
+
   for (var i=0; i<newFiles.length; i++){
     fid = newFiles[i];
     
